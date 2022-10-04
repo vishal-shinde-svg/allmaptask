9cbe6cab14aaad55ee86840832937d597a813fd1
vishal-shinde-svg <108323759+vishal-shinde-svg@users.noreply.github.com>
2022-10-04 14:28:20 +0530

    Add files via upload

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+export default defineNuxtConfig({
+  ssr: false,
+  css: [
+    "mapbox-gl/dist/mapbox-gl.css",
+    "v-mapbox/dist/v-mapbox.css",
+    "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css",
+  ],
+});
